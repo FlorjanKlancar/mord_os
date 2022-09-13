66fde0c53e4e20e6ee776e48bd1a54c78e00d883
FlorjanKlancar <klancar.florjan99@gmail.com>
2022-09-13 18:50:22 +0200

    fixed build error

--- components/Gallery/GalleryViewSkeleton.tsx
+++ components/Gallery/GalleryViewSkeleton.tsx
@@ -8,7 +8,10 @@ function GalleryViewSkeleton() {
       <div className="flex flex-col max-h-[620px] divide-y-2 overflow-y-auto">
         <div>
           {skeletonRows.map((skeletonRow: number) => (
-            <div className="flex justify-between text-base  hover:bg-slate-500 cursor-pointer px-2 py-3 rounded-xl">
+            <div
+              key={skeletonRow}
+              className="flex justify-between text-base  hover:bg-slate-500 cursor-pointer px-2 py-3 rounded-xl"
+            >
               <div className="flex flex-col space-y-5">
                 <div className="w-48 bg-slate-700 h-8 rounded-xl animate-pulse"></div>
                 <div className="w-72 bg-slate-700 h-8 rounded-xl animate-pulse"></div>
